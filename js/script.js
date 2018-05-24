f06cb7bfe217d17fc8c756d4dd003977aeb267d0
luznykarol <karol.rudzinski1990@gmail.com>
2018-05-24 19:05:44 +0200

    6th commit
    mostly fixed media queries

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-
+	//change the burger image to x
 	$('#burger').click(function() {
 		$(this).toggleClass('navigation__burger--open');
+		
 
+		//animate the list opacity after you toggle the burger.
 		if($(window).width() < 800) {
 		if ($('#list').css('opacity') == 0) {
 			$('#list').css('opacity', 1);
@@ -13,27 +15,19 @@ $(document).ready(function() {
 		}};
 	});
 		
-
+	//after clicking the link, burger closes and list slides up/down
 	$('#link').click(function () {
 		$('#list').slideToggle();
 		$('#burger').toggleClass('navigation__burger--open');
 	});
-	
-	var navbarHeight = $('#navbar').outerHeight();
 
-	$('#link').click(function(){
-		$('html, body').animate({
-			scrollTop: $( $(this).attr('href') ).offset().top -navbarHeight
-		}, 1000);
-		return false;
-	});
-
+	//animate the symbol to do a 360 spin
 	$('.symbol-square').click(function() {
 		$(this).toggleClass('spin');
 	});
 
+	//thanks for the tip !
 	$('#more').click(function() {
-		$(this).text(($('#more').text() == 'thx 4 the tip') ? 'more' : 'thx 4 the tip').fadeIn();
-		$(this).toggleClass('small');
+		$(this).text(($('#more').text() == '<3') ? 'more' : '<3').fadeIn();
 	});
 });
